33c403b3954dd29901f88ce6c16c10f31744bf58
cjkarinen20 <cjkarinen20@gmail.com>
2024-10-07 18:33:35 -0400

    NavBar for Desktop and Mobile

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,9 @@ import NavBar from "./Components/NavBar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#192029]">
-      <div class ="container mx-auto px-12 py-4">
-      <HeroSection />
+      <NavBar />
+      <div class ="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
       </div>
     </main>
   );
