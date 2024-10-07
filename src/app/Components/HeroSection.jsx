d63a4a3d0c00f37455e3d5dcd25b90b9c11aee98
cjkarinen20 <cjkarinen20@gmail.com>
2024-10-06 20:01:44 -0400

    Hero Section Completed

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -1,21 +1,46 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from 'react-type-animation';
 
 const HeroSection = () => {
     return (
         <section>
             <div className="grid grid-cols-1 lg:grid-cols-12">
-                <div className="col-span-7 place-self-center">
-                    <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-                        Hello, I am CJ.
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500"> Hello, I'm{" "} </span>
+                        <br></br>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'CJ',
+                                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'a Game Designer',
+                                1000,
+                                'a Web Developer',
+                                1000,
+                                'a Software Engineer',
+                                1000
+                            ]}
+                            wrapper="span"
+                            speed={25}
+                            repeat={Infinity}
+                        />
                     </h1>
-                    <p className="text-[#c0c0c0] text-lg lg:text-xl">
-                        I am a recent Computer Science graduate from Kettering University with over 2 years of tech industry experience in embedded programming, software testing, and systems design.
+                    <p className="text-[#c0c0c0] text-base sm:text-lg mb-6 lg:text-xl">
+                        I am a recent Computer Science graduate from Kettering University with over 3 years of industry experience in embedded programming, software testing, and systems design.
                         Additionally, I have personal and academic experience in game design, level design, environmental modelling, and systems programming.
                     </p>
+                    <div>
+                        <button className="px-6 py-3 w-full sm:w-full rounded-full mr-4 bg-gradient-to-br from-yellow-500 via-orange-500 to-red-500 hover:bg-slate-200 text-black">Hire Me</button>
+                        <button className="px-1 py-1 w-full sm:w-full rounded-full bg-gradient-to-r from-yellow-500 via-orange-500 to-red-500 hover:bg-slate-800  text-white mt-3">
+                            <span className="block bg-[#192029] hover:bg-slate-800 rounded-full px-5 py-2"> Download Resume </span>
+                        </button>
+                    </div>
                 </div>
                 <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-                    <div className="rounded-full bg-[#d4d3d3] w-[375px] h-[375px] lg:w-[400px] lg:h-[400px] relative">
+                    <div className="rounded-full bg-[#c3c2c2] w-[375px] h-[375px] lg:w-[400px] lg:h-[400px] relative">
                         <Image
                             src="/images/OIG4.png"
                             alt="OIG"
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import HeroSection from "./Components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#192029] container mx-auto px-12 py-4">
+    <main className="flex min-h-screen flex-col bg-[#192029]">
+      <div class ="container mx-auto px-12 py-4">
       <HeroSection />
+      </div>
     </main>
   );
 }
