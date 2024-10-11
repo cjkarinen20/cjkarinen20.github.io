ef8216eeed7744662e7fd7e6a636a9f83c93d1d8
cjkarinen20 <cjkarinen20@gmail.com>
2024-10-10 20:13:57 -0400

    Contact Section Foundations

---
+++ src/app/Components/api/send/route.js
@@ -0,0 +1,22 @@
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'CJ <Cjkarinen20@gmail.com>',
+      to: ['cjkarinen20@gmail.com'],
+      subject: 'Hello world',
+      react: <><p>Email Body</p></>,
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
