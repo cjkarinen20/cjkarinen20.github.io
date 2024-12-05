7b2c3efc99657b0a4b9681816731a3466a785242
cjkarinen20 <cjkarinen20@gmail.com>
2024-12-05 12:25:59 -0500

    Removed memoji hero image

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -8,7 +8,13 @@ import { motion } from "framer-motion";
 const HeroSection = () => {
     return (
         <section className="lg:py-16" id="home">
-            <div className="grid grid-cols-1 sm:grid-cols-12">
+            <div className="grid grid-cols-1 sm:grid-cols-12 "
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '30vh',
+              }}>
                 <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
@@ -54,23 +60,6 @@ const HeroSection = () => {
                                 Download Resume
                             </span>
                         </Link>
-                    </div>
-                </motion.div>
-
-                <motion.div
-                    initial={{ opacity: 0, scale: 0.5 }}
-                    animate={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 0.5 }}
-                    className="col-span-4 place-self-center mt-4 lg:mt-0"
-                >
-                    <div className="rounded-full bg-[#c3c2c2] w-[375px] h-[375px] lg:w-[400px] lg:h-[400px] relative">
-                        <Image
-                            src="/images/Hero.png"
-                            alt="OIG"
-                            className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                            width={300}
-                            height={300}
-                        />
                     </div>
                 </motion.div>
             </div>
